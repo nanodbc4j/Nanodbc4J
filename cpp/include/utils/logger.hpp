55cec50bae8811b3d60f125aa0b2330603b21ce0
Michael1297 <38764610+Michael1297@users.noreply.github.com>
2025-08-22 15:10:37 +0300

    improve filename and function extraction for logging macros

--- cpp/include/utils/logger.hpp
+++ cpp/include/utils/logger.hpp
@@ -3,6 +3,18 @@
 #include "utils/string_utils.hpp"
 #include "utils/path_utils.h"
 
+#define WIDEN2(x) L ## x
+#define WIDEN(x) WIDEN2(x)
+
+#if defined(_MSC_VER) || defined(__GNUC__) || defined(__clang__)
+#define __WFUNCTION__ WIDEN(__FUNCTION__)
+#else
+#define __WFUNCTION__ utils::to_wstring(__func__)
+#endif
+
+#define __FILENAME__ utils::extract_filename(__FILE__)
+#define __WFILENAME__ utils::extract_filename(WIDEN(__FILE__))
+
 // Объявление функции инициализации логгера
 void ensure_initialized();
 
@@ -16,20 +28,18 @@ std::string set_spdlog_level(int level = SPDLOG_LEVEL_INFO);
         spdlog::log(level, __VA_ARGS__); \
     } while(0)
 
-#define FMT_STRING_FORMAT(str, ...) fmt::format(fmt::runtime(str), __VA_ARGS__)
-#define __FILENAME__ utils::extract_filename(__FILE__)
 
 // Logging macros
-#define LOG_TRACE(...)    LOG_WITH_INIT(spdlog::level::trace, "[{}:{}] {}():\t{}", __FILENAME__, __LINE__, __func__, FMT_STRING_FORMAT(__VA_ARGS__))
-#define LOG_DEBUG(...)    LOG_WITH_INIT(spdlog::level::debug, "[{}:{}] {}():\t{}", __FILENAME__, __LINE__, __func__, FMT_STRING_FORMAT(__VA_ARGS__))
-#define LOG_INFO(...)     LOG_WITH_INIT(spdlog::level::info,  "[{}:{}] {}():\t{}", __FILENAME__, __LINE__, __func__, FMT_STRING_FORMAT(__VA_ARGS__))
-#define LOG_WARN(...)     LOG_WITH_INIT(spdlog::level::warn,  "[{}:{}] {}():\t{}", __FILENAME__, __LINE__, __func__, FMT_STRING_FORMAT(__VA_ARGS__))
-#define LOG_ERROR(...)    LOG_WITH_INIT(spdlog::level::error, "[{}:{}] {}():\t{}", __FILENAME__, __LINE__, __func__, FMT_STRING_FORMAT(__VA_ARGS__))
-#define LOG_CRITICAL(...) LOG_WITH_INIT(spdlog::level::critical, "[{}:{}] {}():\t{}", __FILENAME__, __LINE__, __func__, FMT_STRING_FORMAT(__VA_ARGS__))
+#define LOG_TRACE(fmt, ...)    LOG_WITH_INIT(spdlog::level::trace, "[{}:{}] {}():\t" fmt, __FILENAME__, __LINE__, __func__, ##__VA_ARGS__)
+#define LOG_DEBUG(fmt, ...)    LOG_WITH_INIT(spdlog::level::debug, "[{}:{}] {}():\t" fmt, __FILENAME__, __LINE__, __func__, ##__VA_ARGS__)
+#define LOG_INFO(fmt, ...)     LOG_WITH_INIT(spdlog::level::info,  "[{}:{}] {}():\t" fmt, __FILENAME__, __LINE__, __func__, ##__VA_ARGS__)
+#define LOG_WARN(fmt, ...)     LOG_WITH_INIT(spdlog::level::warn,  "[{}:{}] {}():\t" fmt, __FILENAME__, __LINE__, __func__, ##__VA_ARGS__)
+#define LOG_ERROR(fmt, ...)    LOG_WITH_INIT(spdlog::level::error, "[{}:{}] {}():\t" fmt, __FILENAME__, __LINE__, __func__, ##__VA_ARGS__)
+#define LOG_CRITICAL(fmt, ...) LOG_WITH_INIT(spdlog::level::critical, "[{}:{}] {}():\t" fmt, __FILENAME__, __LINE__, __func__, ##__VA_ARGS__)
 
-#define LOG_TRACE_W(...)    LOG_WITH_INIT(spdlog::level::trace, L"[{}:{}] {}()", utils::to_wstring(__FILENAME__), __LINE__, utils::to_wstring(__func__), FMT_STRING_FORMAT(__VA_ARGS__))
-#define LOG_DEBUG_W(...)    LOG_WITH_INIT(spdlog::level::debug, L"[{}:{}] {}():\t{}", utils::to_wstring(__FILENAME__), __LINE__, utils::to_wstring(__func__), FMT_STRING_FORMAT(__VA_ARGS__))
-#define LOG_INFO_W(...)     LOG_WITH_INIT(spdlog::level::info,  L"[{}:{}] {}():\t{}", utils::to_wstring(__FILENAME__), __LINE__, utils::to_wstring(__func__), FMT_STRING_FORMAT(__VA_ARGS__))
-#define LOG_WARN_W(...)     LOG_WITH_INIT(spdlog::level::warn,  L"[{}:{}] {}():\t{}", utils::to_wstring(__FILENAME__), __LINE__, utils::to_wstring(__func__), FMT_STRING_FORMAT(__VA_ARGS__))
-#define LOG_ERROR_W(...)    LOG_WITH_INIT(spdlog::level::error, L"[{}:{}] {}():\t{}", utils::to_wstring(__FILENAME__), __LINE__, utils::to_wstring(__func__), FMT_STRING_FORMAT(__VA_ARGS__))
-#define LOG_CRITICAL_W(...) LOG_WITH_INIT(spdlog::level::critical, L"[{}:{}] {}():\t{}", utils::to_wstring(__FILENAME__), __LINE__, utils::to_wstring(__func__), FMT_STRING_FORMAT(__VA_ARGS__))
+#define LOG_TRACE_W(fmt, ...)    LOG_WITH_INIT(spdlog::level::trace, L"[{}:{}] {}():\t" fmt, __WFILENAME__, __LINE__, __WFUNCTION__, ##__VA_ARGS__)
+#define LOG_DEBUG_W(fmt, ...)    LOG_WITH_INIT(spdlog::level::debug, L"[{}:{}] {}():\t" fmt, __WFILENAME__, __LINE__, __WFUNCTION__, ##__VA_ARGS__)
+#define LOG_INFO_W(fmt, ...)     LOG_WITH_INIT(spdlog::level::info,  L"[{}:{}] {}():\t" fmt, __WFILENAME__, __LINE__, __WFUNCTION__, ##__VA_ARGS__)
+#define LOG_WARN_W(fmt, ...)     LOG_WITH_INIT(spdlog::level::warn,  L"[{}:{}] {}():\t" fmt, __WFILENAME__, __LINE__, __WFUNCTION__, ##__VA_ARGS__)
+#define LOG_ERROR_W(fmt, ...)    LOG_WITH_INIT(spdlog::level::error, L"[{}:{}] {}():\t" fmt, __WFILENAME__, __LINE__, __WFUNCTION__, ##__VA_ARGS__)
+#define LOG_CRITICAL_W(fmt, ...) LOG_WITH_INIT(spdlog::level::critical, L"[{}:{}] {}():\t" fmt, __WFILENAME__, __LINE__, __WFUNCTION__, ##__VA_ARGS__)
--- cpp/include/utils/path_utils.h
+++ cpp/include/utils/path_utils.h
@@ -4,11 +4,18 @@
 
 namespace utils {
 
-    constexpr std::string_view extract_filename(std::string_view path) {
-        if (path.empty()) return "";
+    constexpr std::string_view extract_filename(std::string_view path) noexcept {
+        if (path.empty()) return std::string_view{};
 
         // Находим последний слеш или обратный слеш
-        auto last_slash = path.find_last_of("/\\");
+        size_t last_slash = std::string_view::npos;
+        for (size_t i = path.size(); i-- > 0;) {
+            if (path[i] == '/' || path[i] == '\\') {
+                last_slash = i;
+                break;
+            }
+        }
+
         if (last_slash == std::string_view::npos) {
             return path; // Если нет слешей, возвращаем всю строку
         }
@@ -16,9 +23,33 @@ namespace utils {
         return path.substr(last_slash + 1);
     }
 
-    // Перегрузка для C-строк
-    constexpr std::string_view extract_filename(const char* path) {
-        return path ? extract_filename(std::string_view(path)) : "";
+    // Для C - строк(const char*)
+    constexpr std::string_view extract_filename(const char* path) noexcept {
+        return path ? extract_filename(std::string_view(path)) : std::string_view{};
+    }
+
+    constexpr std::wstring_view extract_filename(std::wstring_view path) noexcept {
+        if (path.empty()) return std::wstring_view{};
+
+        // Находим последний слеш или обратный слеш
+        size_t last_slash = std::wstring_view::npos;
+        for (size_t i = path.size(); i-- > 0;) {
+            if (path[i] == L'/' || path[i] == L'\\') {
+                last_slash = i;
+                break;
+            }
+        }
+
+        if (last_slash == std::wstring_view::npos) {
+            return path; // Если нет слешей, возвращаем всю строку
+        }
+
+        return path.substr(last_slash + 1);
+    }
+
+    // Для C-строк (const wchar_t*)
+    constexpr std::wstring_view extract_filename(const wchar_t* path) noexcept {
+        return path ? extract_filename(std::wstring_view(path)) : std::wstring_view{};
     }
 
 } // namespace utils
